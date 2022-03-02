9c630cfe7ea078b77b5bdb53b328b3d60007319a
Pratap29 <pratap291192@gmail.com>
2022-03-02 11:46:37 +0530

    Updatedrabbit.file

---
+++ read1.sh
@@ -0,0 +1,5 @@
+echo "Please enter the DevOps Tools: "
+
+read devOpsTool1 devOpsTool2 devOpsTool3 devOpsTool4 devOpsTool5
+
+`echo The DevOps Tools /Techniques you entereed are $devOpsTool1 $devOpsTool2 $devOpsTool3 $devOpsTool4 $devOpsTool5
